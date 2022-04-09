8f8fc44a561acdb6777c29e35e15864257d7dc5b
Bsully505 <bsully505@yahoo.com>
2022-04-09 13:11:01 -0400

    worked on front end a little

---
+++ Conversion.py
@@ -0,0 +1,19 @@
+class Conversion:
+    dict={}
+    def __init__(self):
+        self.dict['Hello']=''#insert url 
+        self.dict['Goodbye']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+        self.dict['Hello']=''#insert url 
+
+    def GetUrl(self,word):
+        return self.dict[word]
+        
